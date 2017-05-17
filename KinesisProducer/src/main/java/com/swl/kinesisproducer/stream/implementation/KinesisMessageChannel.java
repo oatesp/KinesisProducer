08450e58cac69c7233b519d358cd847f9223365e
Peter.Oates <Peter.Oates@sci-ware.com>
2017-05-17 08:56:31 +0100

    updates

--- KinesisProducer/src/main/java/com/swl/kinesisproducer/stream/implementation/KinesisMessageChannel.java
+++ KinesisProducer/src/main/java/com/swl/kinesisproducer/stream/implementation/KinesisMessageChannel.java
@@ -1,20 +1,35 @@
 package com.swl.kinesisproducer.stream.implementation;
 
+import java.io.UnsupportedEncodingException;
+import java.nio.ByteBuffer;
+
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 
+import com.amazonaws.services.kinesis.producer.KinesisProducer;
+
 public class KinesisMessageChannel implements MessageChannel {
 
+	
+	KinesisProducer kinesis = new KinesisProducer(); 
+	
 	@Override
 	public boolean send(Message<?> message) {
 		
 
+		ByteBuffer data = null;
+		try {
+			data = ByteBuffer.wrap(message.getPayload().toString().getBytes("UTF-8"));
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	          
+	    kinesis.addUserRecord("myStream", "timestamp", data);
 		
 		
-		
-		
-		
-		
+
 		return false;
 	}
 
